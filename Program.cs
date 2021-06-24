d1e39babf451f4d3ae8cd4fc0d382c6649886f1d
sangsik <sanghot1004@gmail.com>
2021-06-24 15:57:02 +0900

    2nd

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FirstProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
