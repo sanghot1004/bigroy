55f4b042368f42f47576a3f51bb8b116c9508ba2
sangsik <sanghot1004@gmail.com>
2021-06-24 15:50:11 +0900

    1st commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
